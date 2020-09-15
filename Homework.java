abcb40d5aa9f983e5db9e3fc56a79ef1b40967db
mt464 <54955071+mt464@users.noreply.github.com>
2020-09-14 20:50:44 -0400

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,23 @@
+public class Homework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+      String name = "Mike";
+
+		// 2. Output the name to the console
+      System.out.println(name);
+
+		// 3. Create a variable that holds an age (as a whole number)
+      int age = 22;
+      
+		// 4. Output the age to the console
+      System.out.println(age);
+      
+		// 5. Create a variable that holds a decimal
+      double dec = 3.141592638579;
+
+		// 6. Output the variable
+      System.out.println(dec);
+      
+	}
+}
